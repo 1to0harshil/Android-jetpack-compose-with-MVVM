fca6575ec53f94541d1e35d5a68edd0e1d29fbf3
SIT72 <harshil.j@shaligraminfotech.com>
2024-01-24 15:01:30 +0530

    hilt added.1

--- app/src/main/java/com/example/ComposeWithMVVM/repository/Repository.kt
+++ app/src/main/java/com/example/ComposeWithMVVM/repository/Repository.kt
@@ -1,6 +1,5 @@
 package com.example.ComposeWithMVVM.repository
 
-import com.example.ComposeWithMVVM.networkModule.RetrofitInstance
 import com.example.ComposeWithMVVM.dataClasses.Login
 import com.example.ComposeWithMVVM.dataClasses.LoginReq
 import com.example.ComposeWithMVVM.interfaces.ProjectServices
